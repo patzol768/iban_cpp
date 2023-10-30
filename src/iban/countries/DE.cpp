a68bb5196522964b8c35918d391457ff6e5e4bf0
patzol768 <zoltan.patocs@yahoo.com>
2023-10-31 00:07:42 +0100

    Add licensing info

--- src/iban/countries/DE.cpp
+++ src/iban/countries/DE.cpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2023 Zoltan Patocs
+ *
+ * Licensed under the MIT License (the "License"). You may not use
+ * this file except in compliance with the License. You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://opensource.org/license/mit/
+ */ 
+
+// https://www.bundesbank.de/de/aufgaben/unbarer-zahlungsverkehr/serviceangebot/pruefzifferberechnung/pruefzifferberechnung-fuer-kontonummern-603282
+
 #include "iban/countries/DE.h"
 #include "iban/bic.h"
 #include "iban/common.h"
